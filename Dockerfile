f43c85feeeac6e780c9e833d12265b62054cc6bd
flaviostutz <flaviostutz@gmail.com>
2020-07-12 01:58:12 -0300

    overall review

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,9 @@ RUN apt-get update && apt-get install -y netcat
 
 ENV CONFIG_REPLICA_SET 'replica-set1'
 ENV CONFIG_SERVER_NODES ''
-ENV SHARD_REPLICA_SET_PREFIX 'shard'
-ENV SHARD_NODES_1 ''
-ENV SHARD_NODES_2 ''
-ENV SHARD_NODES_3 ''
+ENV ADD_SHARD_REPLICA_SET_PREFIX 'shard'
+ENV ADD_SHARD_1_NODES ''
+ENV ADD_SHARD_2_NODES ''
 
 ADD /startup.sh /
 ADD /config.sh /
--- config.sh
+++ config.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "Waiting for local server to be available at 27017..."
+while ! nc -z 127.0.0.1 27017; do sleep 0.5; done
+sleep 1
+
 echo "Generating router config"
 echo ""
-rm /init-router.js
+rm /init-addshards.js
 for i in {1..100}; do 
-    SHARD_REPLICA_SET_NAME="${SHARD_REPLICA_SET_PREFIX}$i"
-    var="SHARD_${i}_NODES"
+    SHARD_REPLICA_SET_NAME="${ADD_SHARD_REPLICA_SET_PREFIX}$i"
+    var="ADD_SHARD_${i}_NODES"
     SNODES="${!var}"
 
     if [ "$SNODES" == "" ]; then
@@ -16,21 +20,17 @@ for i in {1..100}; do
     echo "Shard ${SHARD_REPLICA_SET_NAME} nodes:"
     for N in "${NODES[@]}"; do
         echo "    - $N"
-        echo "sh.addShard(\"$SHARD_REPLICA_SET_NAME/$N:27017\")" >> /init-router.js
+        echo "sh.addShard(\"$SHARD_REPLICA_SET_NAME/$N:27017\")" >> /addshards.js
         CONFIGDB="${CONFIGDB}${S}$N:27017"
         S=","
     done
 done
 echo ""
 
-echo "/init-router.js"
-cat /init-router.js
+echo "/addshards.js"
+cat /addshards.js
 
-echo "Waiting for local server to be available at 27017..."
-while ! nc -z 127.0.0.1 27017; do sleep 0.5; done
-sleep 3
-
-echo "CONFIGURING CLUSTER ROUTER..."
-mongo < /init-router.js
-echo "CONFIGURATION OK"
+echo "ADDING SHARDS TO CLUSTER..."
+mongo < /addshards.js
+echo ">>> SHARDS ADDED SUCCESSFULLY"
 
