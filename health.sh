254a6dd37afe02639177c415b251228c00d75996
flaviostutz <flaviostutz@gmail.com>
2020-08-15 12:13:41 -0300

    refactoring healthcheck

--- health.sh
+++ health.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
-set -e
+function deepCheck() {
+    #check for shard status
+    R=$(echo 'sh.status().ok' | mongo localhost:27017/test --quiet)
+    echo $R | grep 1
+    if [ "$?" == "0" ]; then
+        return 0
+    fi
+    return 1
+}
 
+set -e
 nc -z 127.0.0.1 27017
+set +e
 
 if [ ! -f "/data/__initialized" ]; then
     echo "Node not initialized yet"
-    set +e
-    R=$(echo 'sh.status().ok' | mongo localhost:27017/test --quiet)
-    E=$?
-    if [ "$E" = "0" ]; then
-        if [ "$R" = "1" ]; then
-            touch /data/__initialized
-        fi
+    R=$(deepCheck)
+    if [ "$R" = "0" ]; then
+        touch /data/__initialized
     fi
-
-else
-    echo "Node already initialized. make deep check"
-    #check for shard status
-    R=$(echo 'sh.status().ok' | mongo localhost:27017/test --quiet)
-    echo $R | grep 1 
+    exit 0
 fi
+
+echo "Node already initialized. make deep check"
+R=$(deepCheck)
+exit $R
+
