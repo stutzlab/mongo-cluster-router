673d2de321f27279c22f1964dcd08dae38ef5fc5
flaviostutz <flaviostutz@gmail.com>
2020-08-15 17:35:24 -0300

    simplyfing health check

--- config.sh
+++ config.sh
@@ -19,12 +19,15 @@ for i in {1..100}; do
     S=""
     echo "Shard ${SHARD_REPLICA_SET_NAME} nodes:"
     for N in "${NODES[@]}"; do
-        echo "    - $N"
+        echo ">>> Node $N"
         echo "sh.addShard(\"$SHARD_REPLICA_SET_NAME/$N:27017\")" >> /addshards.js
         CONFIGDB="${CONFIGDB}${S}$N:27017"
         S=","
-        echo "    - Waiting for host $N to be available..."
+        echo " - Waiting for host $N to be available..."
         until ping -c1 $N >/dev/null; do sleep 2; done
+        echo " - Host available. Waiting port 27017..."
+        while ! nc -z $N 27017; do sleep 1; done
+        echo " - Host $N OK"
     done
 done
 echo ""
--- health.sh
+++ health.sh
@@ -1,31 +1,4 @@
 #!/bin/sh
-
-deepCheck() {
-    #check for shard status
-    R=$(echo 'sh.status().ok' | mongo localhost:27017/test --quiet)
-    if [ "$?" != "0" ]; then
-        echo "1"
-        return
-    fi
-    echo $R | grep 1
-    echo $?
-}
-
 set -e
 nc -z 127.0.0.1 27017
-set +e
-
-if [ ! -f "/data/__initialized" ]; then
-    echo "Service is up. shard not initialized yet"
-    E=$(deepCheck)
-    if [ "$E" = "0" ]; then
-        echo "Marking this as initialized"
-        touch /data/__initialized
-    fi
-    exit 0
-fi
-
-echo "Service is up. shard initialized. making deep check"
-R=$(deepCheck)
-exit $R
 
