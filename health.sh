17820e808100176c0b8ee349deab4438d58af656
flaviostutz <flaviostutz@gmail.com>
2020-08-15 13:26:34 -0300

    fixing health

--- health.sh
+++ health.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-function deepCheck() {
+deepCheck() {
     #check for shard status
     R=$(echo 'sh.status().ok' | mongo localhost:27017/test --quiet)
+    if [ "$?" != "0" ]; then
+        echo "1"
+        return
+    fi
     echo $R | grep 1
-    return $?
+    echo $?
 }
 
 set -e
@@ -12,15 +16,16 @@ nc -z 127.0.0.1 27017
 set +e
 
 if [ ! -f "/data/__initialized" ]; then
-    echo "Node not initialized yet"
-    R=$(deepCheck)
-    if [ "$R" = "0" ]; then
+    echo "Service is up. shard not initialized yet"
+    E=$(deepCheck)
+    if [ "$E" = "0" ]; then
+        echo "Marking this as initialized"
         touch /data/__initialized
     fi
     exit 0
 fi
 
-echo "Node already initialized. make deep check"
+echo "Service is up. shard initialized. making deep check"
 R=$(deepCheck)
 exit $R
 
